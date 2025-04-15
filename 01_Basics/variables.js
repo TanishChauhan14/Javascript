e57f3204f2c2d52ba445170a73a001b35f878ce7
Tanish chauhan <161298899+TanishChauhan14@users.noreply.github.com>
2025-04-15 17:06:29 +0000

    all variables add

--- 01_Basics/variables.js
+++ 01_Basics/variables.js
@@ -4,3 +4,26 @@ let city = "Najafgarh";
 village = "Bhiwani";
 
 console.table([student_id,name,city,village]);
+
+// Types of variables 
+
+// number 
+let a = 2;
+//bigint
+let b =120224325657657;
+// String
+let c = "tanish";
+// boolean
+let d = true;
+// null Standalone value
+let e = null;
+// undefined 
+let f = undefined
+// symbol unique in React 
+
+console.log(typeof a+" "+a);
+console.log(typeof b+" "+b);
+console.log(typeof c+" "+c);
+console.log(typeof d+" "+d);
+console.log(typeof e+" "+e);
+
